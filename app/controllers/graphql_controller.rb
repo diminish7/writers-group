2c5498c53dc7472d407ac3d80e2cdb98d8e2ce45
Jason Rush <diminish7@gmail.com>
2020-04-27 16:20:05 -0700

    Turn off CSRF checks for token-auth controllers
    Not using cookies, so CSRF attacks aren't applicable, and this simplifies some stuff

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 class GraphqlController < ApplicationController
-  # If accessing from outside this domain, nullify the session
-  # This allows for outside API access while preventing CSRF attacks,
-  # but you'll have to authenticate your user separately
-  # protect_from_forgery with: :null_session
+  skip_forgery_protection # This is using token auth with local storage, not cookies
 
   def execute
     variables = ensure_hash(params[:variables])
--- app/javascript/src/config/apolloProvider.js
+++ app/javascript/src/config/apolloProvider.js
@@ -5,11 +5,9 @@ const apolloClient = new ApolloClient({
   uri: `${window.location.origin}/graphql`,
   request(operation) {
     const authToken = localStorage.getItem('authToken');
-    const csrfToken = document.querySelector('meta[name="csrf-token"]').attributes.content.value;
 
     operation.setContext({
       headers: {
-        'X-CSRF-Token': csrfToken,
         authorization: authToken ? `Bearer ${authToken}` : '',
       },
     });
